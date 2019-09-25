9db5a860a7f7e6f5dcb5d5ae7c313e45a87f1e16
Caden Atkinson <caden.a383@stu.nebo.edu>
2019-09-25 09:56:02 -0600

    Fixed it

---
+++ create with code/create with code/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    float speed = 0.5f;
+    public float rotationSpeed = 100f;
+    public float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * speed);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.right * rotationSpeed * verticalInput * Time.deltaTime);
+        // barrel roll
+        transform.Rotate(Vector3.forward * rotationSpeed * verticalInput * Time.deltaTime);
+    }
+}
---
+++ create with code/create with code/Assets/Challenge 1/Scripts/PropellerSpin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellerSpin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public int speed = 10;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * speed * Time.deltaTime);
+    }
+}
