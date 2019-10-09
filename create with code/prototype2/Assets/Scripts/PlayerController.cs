ee5bbd0befe6ebb3beb3d1b128669831a0e5cfbc
Caden Atkinson <caden.a383@stu.nebo.edu>
2019-10-09 10:28:43 -0600

    player shooty shoot

--- create with code/prototype2/Assets/Scripts/PlayerController.cs
+++ create with code/prototype2/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 50f;
+    public float xLimit = 10f;
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,16 +19,22 @@ public class PlayerController : MonoBehaviour
     
     {
 
-        if (transform.position.x < -10)
+        if (transform.position.x < -xLimit)
         {
-            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-xLimit, transform.position.y, transform.position.z);
         }
-        else if(transform.position.y < 10)
+        if(transform.position.x > xLimit)
         {
-            transform.position = new Vector3(10, transform.position.y, transform.position.z);
+            transform.position = new Vector3(xLimit, transform.position.y, transform.position.z);
         }
         horizontalInput = Input.GetAxis("Horizontal");
 
         transform.Translate(Vector3.right * Time.deltaTime* horizontalInput * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //When space is pressed
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
