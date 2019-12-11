d4071212f5422aa63ea4d090fa9451c434c8fbf1
Caden Atkinson <caden.a383@stu.nebo.edu>
2019-12-11 10:32:56 -0700

    part 4

---
+++ create with code/prototype 4/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody enemyRb;
+    private GameObject player;
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ create with code/prototype 4/Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public float rotationSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalSpeed = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontalSpeed * rotationSpeed * Time.deltaTime);
+    }
+}
